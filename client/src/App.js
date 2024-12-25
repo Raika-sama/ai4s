8cf40c4be61d442206bf083506a9f48ff7786855
Raika-sama <riccardonicolilb@gmail.com>
2024-12-25 18:40:41 +0100

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import Dashboard from './pages/Dashboard';
+import TestPage from './pages/TestPages';
+import ResultPage from './pages/ResultPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} /> {/* Reindirizza alla pagina di login */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/test/:testId" element={<TestPage />} />
+        <Route path="/results/:resultId" element={<ResultPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- server/app.js
+++ server/app.js
@@ -3,17 +3,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 5000; // Puoi cambiare la porta se necessario
+const port = process.env.PORT || 5000;
+
+// Importa le routes
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const testRoutes = require('./routes/testRoutes');
 const resultRoutes = require('./routes/resultRoutes');
 
-
-
+// Middleware per il parsing del body delle richieste
+app.use(express.json()); 
 
 // Stringa di connessione a MongoDB Atlas
-const uri = "mongodb+srv://RaikaSama:5LxHzpgip4CNxPMx@ai4sdb.7leax.mongodb.net/?retryWrites=true&w=majority&appName=ai4sDB"; 
+const uri = "mongodb+srv://RaikaSama:5LxHzpgip4CNxPMx@ai4sdb.7leax.mongodb.net/?retryWrites=true&w=majority&appName=ai4sDB";
 
 // Connessione a MongoDB
 mongoose.connect(uri, {
@@ -27,19 +29,13 @@ mongoose.connect(uri, {
   console.error('Errore di connessione a MongoDB:', err);
 });
 
-// Definizione di una semplice route per testare il server
-app.get('/', (req, res) => {
-  res.send('Ciao dal server!');
-});
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/tests', testRoutes);
+app.use('/api/results', resultRoutes);
 
 // Avvio del server
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
 });
-
-app.use('/api/auth', authRoutes); // Usa le routes di autenticazione
-app.use('/api/users', userRoutes); // Usa le routes degli utenti
-
-app.use('/api/tests', testRoutes); // Usa le routes dei test. Al momento, questa API restituisce tutti i test presenti nel database. In futuro, potremmo aggiungere la logica per filtrare i test in base al ruolo dell'utente o ad altri criteri.
-
-app.use('/api/results', resultRoutes); // Usa le routes dei risultati
